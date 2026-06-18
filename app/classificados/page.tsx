d4824ce1bf66ed0fd9a6b51af278c72a7603c275
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2026-06-17 21:35:00 -0300

    fix: tipagem breadcrumb

--- app/classificados/page.tsx
+++ app/classificados/page.tsx
@@ -3,7 +3,7 @@ import PageHeader from "@/src/components/PageHeader";
 export default function ClassifiedsPage() {
   return (
     <>
-      <PageHeader title="Classificados" description="Anuncie, encontre e negocie em um só lugar" breadcrumb={["Home", "Classificados"]} handleSearch={() => { }} />
+      <PageHeader title="Classificados" description="Anuncie, encontre e negocie em um só lugar" breadcrumb={[{ label: 'Home', href: '/' }, { label: 'Classificados' }]} handleSearch={() => { }} />
     </>
   );
 }
