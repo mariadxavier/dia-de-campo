8b444217c6921f5a8deea2eb7a536d2f2931af87
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2026-06-17 21:43:05 -0300

    fix: remove conteudo classificados

--- app/classificados/page.tsx
+++ app/classificados/page.tsx
@@ -1,9 +1,6 @@
-import PageHeader from "@/src/components/PageHeader";
-
 export default function ClassifiedsPage() {
   return (
     <>
-      <PageHeader title="Classificados" description="Anuncie, encontre e negocie em um só lugar" breadcrumb={[{ label: 'Home', href: '/' }, { label: 'Classificados' }]} handleSearch={() => { }} />
     </>
   );
 }
