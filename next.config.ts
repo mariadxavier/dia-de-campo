8d1a1e3b8166493d4d4f08edbf2ab8e5ab322611
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2026-06-20 16:49:43 -0300

    melhoria next config imagens externas

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,14 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: "https",
         hostname: "picsum.photos",
+        port: '',
+        pathname: '/**',
       },
     ],
   },
