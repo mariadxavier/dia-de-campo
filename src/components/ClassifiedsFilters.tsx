a23356d6e6ce9633f1981a9c9f9ef4bff3a1d280
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2026-07-06 20:24:36 -0300

    style: adequacoes visuais pagina de classificados para desktop

--- src/components/ClassifiedsFilters.tsx
+++ src/components/ClassifiedsFilters.tsx
@@ -55,51 +55,56 @@ export default function ClassifiedsFilters({
   };
 
   return (
-    <section className="flex flex-col w-full gap-4 md:gap-6 py-9 px-5 md:p-8">
+    <section className="flex flex-col w-full gap-4 md:gap-6 py-2 px-5 md:p-8 lg:p-16">
       <div className="flex flex-col gap-1">
-        <h1 className="font-bold text-2xl text-(--color-dark-blue)">Encontre o anúncio certo</h1>
+        <h1 className="font-bold text-2xl text-(--color-dark-blue) lg:text-3xl">Encontre o anúncio certo</h1>
         <p className="text-(--color-gray) text-sm lg:text-base">
           Filtre por categoria e localização.
         </p>
       </div>
-      <p className="font-semibold txt-(--color-dark-blue) text-sm">Categorias</p>
-      <div className="flex gap-1.5 flex-wrap">
-        {categories.map((tab) => {
-          const isActive = activeCategory === tab;
-          return (
-            <button
-              key={tab}
-              id={`classifieds-category-${tab}`}
-              type="button"
-              onClick={() => onCategoryChange(tab)}
-              className={`
+      <div className='flex flex-col gap-4 md:gap-6 lg:flex-row lg:gap-18'>
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold txt-(--color-dark-blue) text-sm">Categorias</p>
+          <div className="flex gap-1.5 flex-wrap">
+            {categories.map((tab) => {
+              const isActive = activeCategory === tab;
+              return (
+                <button
+                  key={tab}
+                  id={`classifieds-category-${tab}`}
+                  type="button"
+                  onClick={() => onCategoryChange(tab)}
+                  className={`
                 flex items-center gap-2 px-3 py-2 rounded-full text-xs font-semibold whitespace-nowrap shrink-0 transition-colors cursor-pointer
-                ${
-                  isActive
-                    ? 'bg-(--color-green) text-(--color-white)'
-                    : 'border border-(--color-gray)/30 text-(--color-dark-gray) hover:border-(--color-green) hover:text-(--color-green)'
-                }
+                ${isActive
+                      ? 'bg-(--color-green) text-(--color-white)'
+                      : 'border border-(--color-gray)/30 text-(--color-dark-gray) hover:border-(--color-green) hover:text-(--color-green)'
+                    }
               `}
+                >
+                  {tab.toLocaleUpperCase()}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 rounded-lg bg-(--color-white-dust) border border-(--color-light-gray) p-3 lg:p-6 lg:w-1/2 max-w-[500px]">
+          <p className="font-semibold txt-(--color-dark-blue) text-sm">Localização</p>
+          <div className="cursor-pointer bg-(--color-light-green) max-w-[200px] px-4 py-2 border border-(--color-green) rounded-full text-(--color-green)">
+            <select
+              className="cursor-pointer w-full border-none font-semibold text-[13px] outline-none"
+              value={activeState}
+              onChange={onStateChange}
             >
-              {tab.toLocaleUpperCase()}
-            </button>
-          );
-        })}
-      </div>
-      <p className="font-semibold txt-(--color-dark-blue) text-sm">Localização</p>
-      <div className="cursor-pointer bg-(--color-light-green) w-fit px-4 py-2 border border-(--color-green) rounded-full text-(--color-green)">
-        <select
-          className="cursor-pointer w-full border-none font-semibold text-[13px] outline-none"
-          value={activeState}
-          onChange={onStateChange}
-        >
-          {states &&
-            states.map((item, idx) => (
-              <option key={idx} value={item}>
-                {item}
-              </option>
-            ))}
-        </select>
+              {states &&
+                states.map((item, idx) => (
+                  <option key={idx} value={item}>
+                    {item}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/ClassifiedsList.tsx
+++ src/components/ClassifiedsList.tsx
@@ -10,20 +10,23 @@ export default function ClassifiedsList({
   totalCount: number;
 }) {
   return (
-    <section className="flex flex-col w-full gap-4 md:gap-8 py-9 px-5 md:p-8">
-      <div className="flex flex-col gap-1">
-        <h1 className="font-bold text-2xl text-(--color-dark-blue)">Anúncios em destaque</h1>
-        <p className="text-(--color-gray) text-sm lg:text-base">
-          Itens e oportunidades selecionados
-        </p>
+    <section className="flex flex-col w-full gap-4 md:gap-8 py-9 px-5 md:p-8 lg:px-18">
+      <div className='flex flex-col gap-1 md:flex-row md:justify-between'>
+        <div className="flex flex-col gap-1">
+          <h1 className="font-bold text-2xl text-(--color-dark-blue) lg:text-4xl">Anúncios em destaque</h1>
+          <p className="text-(--color-gray) text-sm lg:text-base">
+            Itens e oportunidades selecionados
+          </p>
+        </div>
+        <div
+          className={
+            'rounded-lg w-fit p-2 lg:p-4 mt-2 font-bold flex justify-center items-center bg-(--color-white-shell) text-(--color-dark-green) border border-(--color-light-gray)'
+          }
+        >{`${totalCount} anúncios ativos no total`}</div>
+
       </div>
-      <div
-        className={
-          'rounded-lg w-fit p-2 mt-2 font-bold flex justify-center items-center bg-(--color-white-shell) text-(--color-dark-green) border border-(--color-light-gray)'
-        }
-      >{`${totalCount} anúncios ativos no total`}</div>
       {classifieds.length > 0 ? (
-        <div className="flex flex-col gap-4 md:gap-8 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="flex flex-col gap-4 md:gap-8 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {classifieds.map((article) => (
             <ClassifiedsPageArticle key={article.id} article={article} />
           ))}
